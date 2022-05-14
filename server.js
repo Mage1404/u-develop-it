6ac98e89c54a3805ef8a04ba5d5e956b1e8c91bb
Luyan Zhang <z709973733@gmail.com>
2022-05-14 02:02:09 -0400

    update connect

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const inquirer = require('inquirer');
 const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
 
@@ -13,15 +14,24 @@ app.use(express.json());
 app.use('/api', apiRoutes);
 
 // Default response for any other request (Not Found)
-app.use((req, res) => { 
+app.use((req, res) => {
   res.status(404).end();
 });
 
 // Start server after DB connection
 db.connect(err => {
   if (err) throw err;
-  console.log('Database connected.');
+  console.log('----------------');
+  console.log('Employee Manager');
+  console.log('----------------');
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    inquirer.prompt(
+      {
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do? (User arrow keys)',
+        choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+      }
+    ).then(({ action }) => {console.log("you picked : " + action)});
   });
 });
